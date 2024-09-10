2c0d62d3a84b25cdd3561c785ead6a35f58c7f43
Banan_1984 <niketozzz@gmail.com>
2024-09-10 23:45:09 +0300

    Changed getNames func to private, deleted the useless init

--- ExampleProject/Helper.swift
+++ ExampleProject/Helper.swift
@@ -2,13 +2,13 @@ import Foundation
 
 class Helper {
     
-    private var people: [Person]
+    private var people: [Person] = []
     
     public func addName(firstName: String, lastName: String) {
         people.append(Person(firstName: firstName, lastName: lastName))
     }
     
-    public func getNames() -> [String] {
+    private func getNames() -> [String] {
         var fullNames: [String] = []
         for person in people {
             fullNames.append(person.getFullName())
@@ -20,9 +20,5 @@ class Helper {
         for person in getNames(){
             print(person)
         }
-    }
-    
-    init() {
-        people = []
     }
 }
