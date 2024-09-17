f3bc3ef10d925b903bd7ec54a90c74b3cf059d64
Banan_1984 <niketozzz@gmail.com>
2024-09-17 20:07:55 +0300

    Title with fullName and button have been added

--- ExampleProject/ViewController.swift
+++ ExampleProject/ViewController.swift
@@ -8,14 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let textLabel = UILabel()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Show fullName", for: .normal)
+        button.backgroundColor = .green 
+        button.frame = CGRect(x: 100, y: 150, width: 150, height: 50)
+        return button
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .magenta
+        view.backgroundColor = .lightGray
         view.alpha = 0.9
         
         printPeople()
+        
+        setupLabel()
+        view.addSubview(textLabel)
+        view.addSubview(button)
     }
     
     private func printPeople(){
@@ -28,6 +42,14 @@ class ViewController: UIViewController {
         let people: UserRepository = UserRepository()
         
         return people.getUsers()
+    }
+    
+    private func setupLabel(){
+        let randomPerson = getPeopleArray().randomElement()?.getName().fullName
+        textLabel.text = "\(randomPerson ?? "No name")";
+        textLabel.font = .systemFont(ofSize: 25, weight: .regular)
+        textLabel.textColor = .blue
+        textLabel.frame = CGRect(x: 100, y: 100, width: 200, height: 50) // увеличил самостоятельно ширину, тк имена не влезали в это поле
     }
 }
 
